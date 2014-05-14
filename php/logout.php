f7905d834ace482a45986585e5be1b11c17362ef
mauromattos00 <mauromattos00@gmail.com>
2014-05-14 18:12:07 +0000

    Alteração na configuração de cookie Issue 3

--- php/logout.php
+++ php/logout.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 session_destroy();
-setcookie("manterLogado", "", time() - 7200, '/site/');
-setcookie('email', '', time() - 7200, '/site/');
+setcookie("manterLogado", "", time()-60*60, '/site/');
+setcookie('email', "", time()-60*60, '/site/');
 header('Location: ../index.php');
 exit;
 ?>
