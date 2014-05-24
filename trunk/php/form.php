fac23f15e1dab1fca9484f03d721a0d10fedcf4f
mauromattos00@gmail.com <mauromattos00@gmail.com@a50d1f8c-2995-2055-9274-b40652f596fe>
2014-05-24 02:34:07 +0000

    Novos campos no formulário de cadastro.
    Issue 2

--- trunk/php/form.php
+++ trunk/php/form.php
@@ -25,6 +25,10 @@ if (isset($_GET['id_usuario']) && ($id_usuario = $_GET['id_usuario'])) {
         $nome = $row['nome'];
         $sobrenome = $row['sobrenome'];
         $email = $row['email'];
+        $nascimento = $row['nascimento'];
+        $sexo = $row['sexo'];
+        $estado = $row['estado'];
+        $cidade = $row['cidade'];
         $senha = $row['senha'];
     }
 }
@@ -38,72 +42,28 @@ if (isset($_GET['id_usuario']) && ($id_usuario = $_GET['id_usuario'])) {
 
 <form action="php/process.php<?php echo isset($id_usuario) ? '?id_usuario=' . $id_usuario : '' ?>" method="POST">
     <label for="nome">Nome</label>
-    <input type="text" name="nome" id="nome" value="<?php echo isset($nome) ? $nome : '' ?>" />
+    <input type="text" id="nome" value="<?php echo isset($nome) ? $nome : '' ?>" />
 
     <label for="sobrenome">Sobrenome</label>
     <input type="text" id="sobrenome" value="<?php echo isset($sobrenome) ? $sobrenome : '' ?>" />
 
     <label for="email">E-mail</label>
-    <input type="text"  name="email" id="email" value="<?php echo isset($email) ? $email : '' ?>" />
+    <input type="text" id="email" value="<?php echo isset($email) ? $email : '' ?>" />
 
-    <?php if (!taLogado()): ?>
-        <label for="nascimento">Data de nascimento</label>
-        <span>Dia</span>
-        <select>
-            <?php
-            for ($i = 1; $i <= 31; $i++) {
-                print '"<option value="' . $i . '">' . $i . '</option>';
-            }
-            ?>
-        </select>
-        <span>Mês</span>
-        <select>
-            <?php
-            $mes = array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro");
-            for ($i = 0; $i <= 12; $i++) {
-                print '"<option value="' . $i . '">' . $mes[$i] . '</option>';
-            }
-            ?>
-        </select>
+    <label for="nascimento">Data de nascimento (dd/mm/aaaa)</label>
+    <input type="text" id="nascimento" value="<?php echo isset($nascimento) ? $nascimento : '' ?>" />
 
-        <span>Ano</span>            
-        <select>
-            <?php
-            $a = date("Y");
-            for ($i = $a - 110; $i < $a; $i++) {
-                print '"<option value="' . $i . '">' . $i . '</option>';
-            }
-            ?>
-        </select>
-
-
-        <label for="sexo">Sexo</label>
-        <select>
-            <option value="">Selecionar</option>
-            <option value="feminino">Feminino</option>
-            <option value="Masculino">Masculino</option>        
-        </select>
-    <?php endif; ?>
+    <label for="sexo">Sexo</label>
+    <input type="text" id="sexo" value="<?php echo isset($sexo) ? $sexo : '' ?>" />
 
     <label for="estado">Estado</label>
-    <input type="text" id="estado" name="estado" value="<?php echo isset($estado) ? $estado : '' ?>"/>
+    <input type="text" id="estado" value="<?php echo isset($estado) ? $estado : '' ?>"/>
 
     <label for="cidade">Cidade</label>
-    <input type="cidade"  id="cidade" name="cidade" value="<?php echo isset($cidade) ? $cidade : '' ?>"/>
-
-    <label for="ensinoMedio">Ensino Médio</label>
-    <input type="text"  id="ensinoMedio" name="ensinoMedio" value="<?php echo isset($ensinoMedio) ? $ensinoMedio : '' ?>"/>
-
-    <label for="ensinoSuperior">Ensino Superior</label>
-    <input type="text"  id="ensinoSuperior" name="ensinoSuperior" value="<?php echo isset($ensinoSuperior) ? $ensinoSuperior : '' ?>"/>
+    <input type="text"  id="cidade" value="<?php echo isset($cidade) ? $cidade : '' ?>"/>
 
     <label for="Senha">Senha</label>
-    <input type="password" maxlength="15" id="Senha" name="Senha" value="<?php echo isset($senha) ? $senha : '' ?>"/>
-
-    <label for="confirmarSenha">Confirmar Senha</label>
-    <input type="password"  maxlength="15" id="confirmarSenha" name="confirmarSenha" value="<?php echo isset($senha) ? $senha : '' ?>"/>
+    <input type="password" maxlength="15" id="senha" value="<?php echo isset($senha) ? $senha : '' ?>"/>
 
     <input type="submit" id="submit" value="Salvar" /><br>
-
-    <a href="index.php" title="Cancelar">Cancelar</a>
 </form>
--- trunk/php/process.php
+++ trunk/php/process.php
@@ -5,21 +5,21 @@ include'connection.php';
 $nome = isset($_POST['nome']) && $_POST['nome'] ? trim($_POST['nome']) : null;
 $sobrenome = isset($_POST['sobrenome']) && $_POST['sobrenome'] ? trim($_POST['sobrenome']) : null;
 $email = isset($_POST['email']) && $_POST['email'] ? trim($_POST['email']) : null;
+$nascimento = isset($_POST['nascimento']) && $_POST['nascimento'] ? trim($_POST['nascimento']) : null;
+var_dump($nome);
+/*
+$sexo = isset($_POST['sexo']) && $_POST['sexo'] ? trim($_POST['sexo']) : null;
+$estado = isset($_POST['estado']) && $_POST['estado'] ? trim($_POST['estado']) : null;
+$cidade = isset($_POST['cidade']) && $_POST['cidade'] ? trim($_POST['cidade']) : null;
 $senha = isset($_POST['senha']) && $_POST['senha'] ? trim($_POST['senha']) : null;
-$confirmarSenha = isset($_POST['confirmarSenha']) && $_POST['confirmarSenha'] ? trim($_POST['confirmarSenha']) : null;
 
-if ($senha <> $confirmarSenha) {
-    header('Location: ../index.php?pagina=form&message=3');
-    exit;
-}
-
-if ($nome && $sobrenome && $email && $senha) {
-    $sql = (isset($_GET['id_usuario']) && ($id_usuario = $_GET['id_usuario']))
-            ? "UPDATE usuario SET nome='$nome', sobrenome='$sobrenome', email='$email',senha=MD5('$senha') WHERE id_usuario = $id_usuario"
-            : "INSERT INTO usuario (nome, sobrenome, email, senha) VALUES ('$nome', '$sobrenome', '$email', '$senha')";
+if ($nome && $sobrenome && $email && $nascimento && $sexo && $estado && $cidade && $senha){
     
-    $result = mysql_query($sql, $database);
+    $sql = (isset($_GET['id_usuario']) && ($id_usuario = $_GET['id_usuario']))
+            ? "UPDATE usuario SET nome='$nome',sobrenome='$sobrenome',email='$email',nascimento='$nascimento',sexo='$sexo',cidade='$cidade',estado='$estado',senha='$senha' WHERE id_usuario = $id_usuario" 
+            : "INSERT INTO usuario (nome,sobrenome,email,nascimento,sexo,cidade,estado,senha) VALUES ('$nome','$sobrenome','$email',$nascimento,'$sexo','$cidade','$estado','$senha')";
 
+    $result = mysql_query($sql, $database);
     if ($result) {
         header('Location: ../index.php?message=1');
         exit;
@@ -31,5 +31,4 @@ if ($nome && $sobrenome && $email && $senha) {
     header('Location: ../index.php?pagina=form&message=1');
     exit;
 }
-?>
-
+?>*/
