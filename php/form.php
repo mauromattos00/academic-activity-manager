fa7dc0bd8f818fc4558e70ad816339ef12a69423
mauromattos00 <mauromattos00@gmail.com>
2014-05-25 19:00:53 +0000

    Novos campos do formulário de cadastro disponíveis; Correção de validação
    Issue 2

--- php/form.php
+++ php/form.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_GET['message'])) {
     switch ($_GET['message']) {
-        case 1:
+        case 5:
             echo '<p>Todos os campos devem ser preenchidos!</p>';
             break;
         case 2:
@@ -25,11 +25,13 @@ if (isset($_GET['id_usuario']) && ($id_usuario = $_GET['id_usuario'])) {
         $nome = $row['nome'];
         $sobrenome = $row['sobrenome'];
         $email = $row['email'];
+        $senha = $row['senha'];
         $nascimento = $row['nascimento'];
         $sexo = $row['sexo'];
         $estado = $row['estado'];
         $cidade = $row['cidade'];
-        $senha = $row['senha'];
+        $ensinoMedio = $row['ensinoMedio'];
+        $ensinoSuperior = $row['ensinoSuperior'];
     }
 }
 ?>
@@ -42,28 +44,34 @@ if (isset($_GET['id_usuario']) && ($id_usuario = $_GET['id_usuario'])) {
 
 <form action="php/process.php<?php echo isset($id_usuario) ? '?id_usuario=' . $id_usuario : '' ?>" method="POST">
     <label for="nome">Nome</label>
-    <input type="text" id="nome" value="<?php echo isset($nome) ? $nome : '' ?>" />
+    <input type="text" name="nome" id="nome" value="<?php echo isset($nome) ? $nome : '' ?>" />
 
     <label for="sobrenome">Sobrenome</label>
-    <input type="text" id="sobrenome" value="<?php echo isset($sobrenome) ? $sobrenome : '' ?>" />
+    <input type="text" name="sobrenome" id="sobrenome" value="<?php echo isset($sobrenome) ? $sobrenome : '' ?>" />
 
     <label for="email">E-mail</label>
-    <input type="text" id="email" value="<?php echo isset($email) ? $email : '' ?>" />
+    <input type="text" name="email" id="email" value="<?php echo isset($email) ? $email : '' ?>" />
+    
+    <label for="Senha">Senha</label>
+    <input type="password" maxlength="15" name="senha" id="senha" value="<?php echo isset($senha) ? $senha : '' ?>"/>
 
     <label for="nascimento">Data de nascimento (dd/mm/aaaa)</label>
-    <input type="text" id="nascimento" value="<?php echo isset($nascimento) ? $nascimento : '' ?>" />
+    <input type="text" name="nascimento" id="nascimento" value="<?php echo isset($nascimento) ? $nascimento : '' ?>" />
 
     <label for="sexo">Sexo</label>
-    <input type="text" id="sexo" value="<?php echo isset($sexo) ? $sexo : '' ?>" />
+    <input type="text" name="sexo" id="sexo" value="<?php echo isset($sexo) ? $sexo : '' ?>" />
 
     <label for="estado">Estado</label>
-    <input type="text" id="estado" value="<?php echo isset($estado) ? $estado : '' ?>"/>
+    <input type="text" name="estado" id="estado" value="<?php echo isset($estado) ? $estado : '' ?>"/>
 
     <label for="cidade">Cidade</label>
-    <input type="text"  id="cidade" value="<?php echo isset($cidade) ? $cidade : '' ?>"/>
+    <input type="text" name="cidade"  id="cidade" value="<?php echo isset($cidade) ? $cidade : '' ?>"/>
 
-    <label for="Senha">Senha</label>
-    <input type="password" maxlength="15" id="senha" value="<?php echo isset($senha) ? $senha : '' ?>"/>
+    <label for="ensinoMedio">Ensino Médio</label>
+    <input type="text" name="ensinoMedio"  id="ensinoMedio" value="<?php echo isset($ensinoMedio) ? $ensinoMedio : '' ?>"/>
+
+    <label for="ensinoSuperior">Ensino Superior</label>
+    <input type="text" name="ensinoSuperior"  id="ensinoSuperior" value="<?php echo isset($ensinoSuperior) ? $ensinoSuperior : '' ?>"/>
 
     <input type="submit" id="submit" value="Salvar" /><br>
 </form>
