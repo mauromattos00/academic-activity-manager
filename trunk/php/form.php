be62dbb0594db116c4fc55692f5c43717f2f6c29
mauromattos00@gmail.com <mauromattos00@gmail.com@a50d1f8c-2995-2055-9274-b40652f596fe>
2014-06-04 00:29:59 +0000

    Refatoração do código Issue 2

--- trunk/php/form.php
+++ trunk/php/form.php
@@ -16,7 +16,7 @@ if (isset($_GET['message'])) {
     }
 }
 
-if (isset($_GET['id_usuario']) && ($id_usuario = $_GET['id_usuario'])) {
+if (isset($_SESSION['id_usuario']) && ($id_usuario = $_SESSION['id_usuario'])) {
     $sql = "SELECT * FROM usuario WHERE id_usuario = $id_usuario";
     $result = mysql_query($sql, $database);
 
@@ -36,42 +36,50 @@ if (isset($_GET['id_usuario']) && ($id_usuario = $_GET['id_usuario'])) {
 }
 ?>
 
-<?php if (isset($id_usuario)): ?>
-    <h1>Editar Dados</h1>
-<?php else: ?>
-    <h1>Criar uma conta</h1>
-<?php endif; ?>
+<section id="formulario">
+    <?php if (isset($id_usuario)): ?>
+        <h1>Editar Dados</h1>
+    <?php else: ?>
+        <h1>Criar uma conta</h1>
+    <?php endif; ?>
 
-<form action="php/process.php<?php echo isset($id_usuario) ? '?id_usuario=' . $id_usuario : '' ?>" method="POST">
-    <label for="nome">Nome</label>
-    <input type="text" name="nome" id="nome" value="<?php echo isset($nome) ? $nome : '' ?>" />
+    <section id="formEsquerda">
+        <form id="form" action="php/process.php<?php echo isset($id_usuario) ? '?id_usuario=' . $id_usuario : '' ?>" method="POST">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="nome" value="<?php echo isset($nome) ? $nome : '' ?>" />
 
-    <label for="sobrenome">Sobrenome</label>
-    <input type="text" name="sobrenome" id="sobrenome" value="<?php echo isset($sobrenome) ? $sobrenome : '' ?>" />
+            <label for="sobrenome">Sobrenome</label>
+            <input type="text" name="sobrenome" id="sobrenome" value="<?php echo isset($sobrenome) ? $sobrenome : '' ?>" />
 
-    <label for="email">E-mail</label>
-    <input type="text" name="email" id="email" value="<?php echo isset($email) ? $email : '' ?>" />
-    
-    <label for="Senha">Senha</label>
-    <input type="password" maxlength="15" name="senha" id="senha" value="<?php echo isset($senha) ? $senha : '' ?>"/>
+            <label for="email">E-mail</label>
+            <input type="text" name="email" id="email" value="<?php echo isset($email) ? $email : '' ?>" />
 
-    <label for="nascimento">Data de nascimento (dd/mm/aaaa)</label>
-    <input type="text" name="nascimento" id="nascimento" value="<?php echo isset($nascimento) ? $nascimento : '' ?>" />
+            <label for="Senha">Senha</label>
+            <input type="password" maxlength="15" name="senha" id="senha" value="<?php echo isset($senha) ? $senha : '' ?>"/>
 
-    <label for="sexo">Sexo</label>
-    <input type="text" name="sexo" id="sexo" value="<?php echo isset($sexo) ? $sexo : '' ?>" />
+            <label for="nascimento">Data de nascimento (dd/mm/aaaa)</label>
+            <input type="text" name="nascimento" id="nascimento" value="<?php echo isset($nascimento) ? $nascimento : '' ?>" />
 
-    <label for="estado">Estado</label>
-    <input type="text" name="estado" id="estado" value="<?php echo isset($estado) ? $estado : '' ?>"/>
+    </section>
+    <section id="formDireita">
+        <label for="sexo">Sexo</label>
+        <option>
+        <input type="text" name="sexo" id="sexo" value="<?php echo isset($sexo) ? $sexo : '' ?>" />
 
-    <label for="cidade">Cidade</label>
-    <input type="text" name="cidade"  id="cidade" value="<?php echo isset($cidade) ? $cidade : '' ?>"/>
+        <label for="estado">Estado</label>
+        <input type="text" name="estado" id="estado" value="<?php echo isset($estado) ? $estado : '' ?>"/>
 
-    <label for="ensinoMedio">Ensino Médio</label>
-    <input type="text" name="ensinoMedio"  id="ensinoMedio" value="<?php echo isset($ensinoMedio) ? $ensinoMedio : '' ?>"/>
+        <label for="cidade">Cidade</label>
+        <input type="text" name="cidade"  id="cidade" value="<?php echo isset($cidade) ? $cidade : '' ?>"/>
 
-    <label for="ensinoSuperior">Ensino Superior</label>
-    <input type="text" name="ensinoSuperior"  id="ensinoSuperior" value="<?php echo isset($ensinoSuperior) ? $ensinoSuperior : '' ?>"/>
+        <label for="ensinoMedio">Ensino Médio</label>
+        <input type="text" name="ensinoMedio"  id="ensinoMedio" value="<?php echo isset($ensinoMedio) ? $ensinoMedio : '' ?>"/>
 
+        <label for="ensinoSuperior">Ensino Superior</label>
+        <input type="text" name="ensinoSuperior"  id="ensinoSuperior" value="<?php echo isset($ensinoSuperior) ? $ensinoSuperior : '' ?>"/>
+    </section>
+</section>
+<div class="botoes">
     <input type="submit" id="submit" value="Salvar" /><br>
+</div>
 </form>
