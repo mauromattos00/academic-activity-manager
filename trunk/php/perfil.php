4af4fece9f7f840f23cf08f79d37337da9667470
mauromattos00@gmail.com <mauromattos00@gmail.com@a50d1f8c-2995-2055-9274-b40652f596fe>
2014-06-04 00:31:06 +0000

    Exibição do perfil alterado Issue 15

--- trunk/php/perfil.php
+++ trunk/php/perfil.php
@@ -19,43 +19,45 @@ if (isset($_SESSION['id_usuario']) && ($id_usuario = $_SESSION['id_usuario'])) {
 ?>
 
 <html>
-    <h1>Meu perfil</h1>
+    <section id="dados">
+        <img src="img/perfil.jpg" height="200" width="150"> <br/>
+        <ul id="imgPerfil2">
+            <li>
+                <a href="#" class="submenu"><p>Adicionar Imagem de Perfil</p></a>
+                <ul id="imgPerfil">
+                    <li>
+                        <form action="php/imgProcess.php"<?php echo isset($id_usuario) ? '?id_usuario=' . $id_usuario : '' ?>" method="POST">
+                            <input type="file" id="userImage">
+                            <input type="Submit" id="submit" value="Enviar Imagem">
+                        </form>
+                    </li>
+                </ul>
+            </li>
+        </ul>
 
-    <img src="img/perfil.jpg" height="200" width="150"> <br/>
-    <ul id="imgPerfil2">
-        <li>
-            <a href="#" class="submenu"><p>Adicionar Imagem de Perfil</p></a>
-            <ul id="imgPerfil">
-                <li>
-                    <form action="php/imgProcess.php"<?php echo isset($id_usuario) ? '?id_usuario=' . $id_usuario : '' ?>" method="POST">
-                        <input type="file" id="userImage">
-                        <input type="Submit" id="submit" value="Enviar Imagem">
-                    </form>
-                </li>
-            </ul>
-        </li>
-    </ul>
+        <p>
+            <?php echo $nome . " " . $sobrenome ?>
+        </p>    
+        <p>
+            Moro em <?php echo $cidade ?>
+        </p>    
+        <p>
+            <?php echo $estado ?>
+        </p>
+        <p>
+            Sexo <?php echo $sexo ?>
+        </p>
+        <p>
+            Estudou na instituição <a href="#"><b><?php echo $ensinoMedio ?></b></a>
+        </p>
+        <p>
+            Estudou na instituição <a href="#"><b><?php echo $ensinoSuperior ?></b></a>
+        </p>
+        
+        <a href="index.php?pagina=form" title="Editar minha conta"><b>Editar Conta</b></a>
+    </section>
 
-    <p>
-        <?php echo $nome . " " . $sobrenome ?>
-    </p>    
-    <p>
-        Moro em <?php echo $cidade ?>
-    </p>    
-    <p>
-        <?php echo $estado ?>
-    </p>
-    <p>
-        Sexo <?php echo $sexo ?>
-    </p>
-    <p>
-        Estudou na instituição <a href="#"><b><?php echo $ensinoMedio ?></b></a>
-    </p>
-    <p>
-        Estudou na instituição <a href="#"><b><?php echo $ensinoSuperior ?></b></a>
-    </p>
 
-    <input type="button" id="add" value="Adicionar Amigo" />
 </html>
 
 
