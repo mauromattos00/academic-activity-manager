74af43edf7b35246dddd58fdc84b5fd119e90eb4
mauromattos00@gmail.com <mauromattos00@gmail.com@a50d1f8c-2995-2055-9274-b40652f596fe>
2014-06-04 00:29:16 +0000

    Formulário de cadastro de disciplina(incompleto) Issue 7

--- trunk/php/disciplinas.php
+++ trunk/php/disciplinas.php
@@ -1,6 +1,17 @@
 <?php
-include 'php/connection.php';
+include'connection.php';
+
+if (isset($_SESSION['id_usuario']) && ($id_usuario = $_SESSION['id_usuario'])) {
+    $sql = "SELECT * FROM usuario WHERE id_usuario = $id_usuario";
+    $result = mysql_query($sql, $database);
+
+    if (mysql_num_rows($result) == 1) {
+        $row = mysql_fetch_assoc($result);
+        $id_usuario = $row['id_usuario'];
+    }
+}
 ?>
 
-<a href="index.php?pagina=formDisciplina<?php echo isset($id_usuario) ? '?id_usuario=' . $id_usuario : '' ?>">Adicionar disciplina</a>
-
+<html>
+    <a href="index.php?pagina=formDisciplina&id_usuario=<?php echo $_SESSION['id_usuario'] ?>">Adicionar Disciplina</a>
+</html>
