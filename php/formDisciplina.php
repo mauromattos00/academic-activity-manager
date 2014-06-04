28f3d0e627a4ce796b9a134bf43d807507f282bf
mauromattos00 <mauromattos00@gmail.com>
2014-06-04 00:29:16 +0000

    Formulário de cadastro de disciplina(incompleto) Issue 7

--- php/formDisciplina.php
+++ php/formDisciplina.php
@@ -1,10 +1,25 @@
-<h2>Adicionar Disciplina</h2>
+<?php
+if (isset($_GET['message'])) {
+    switch ($_GET['message']) {
+        case 1:
+            echo "Disciplina Adicionada com Sucesso.";
+            break;
+        case 2:
+            echo "Erro ao conectar com o banco de dados.";
+            break;
+        case 3:
+            echo "Dê um nome à nova disciplina!";
+            break;
+    }
+}
+?>
 
-<form action="php/processDisciplina.php<?php echo isset($id_usuario) ? '?id_usuario=' . $id_usuario : '' ?>" method="post">
-<label for="nome_disciplina">Nome da Nova Disciplina</label>
-<input type="text" name="nome_disciplina" id="nome_disciplina">
+<form action="php/processDisciplina.php<?php echo isset($id_usuario) ? '?id_usuario=' . $id_usuario : '' ?>" method="POST">
+    <label for="nomeDisciplina">Nome da disciplina</label>
+    <input type="text" name="nomeDisciplina" id="nomeDisciplina" />
 
-<label for="nome_professor">Nome do(a) Professor(a)</label>
-<input type="text" name="nome_professor" id="nome_professor">
+    <label for="nomeProfessor">Nome do(a) professor(a)</label>
+    <input type="text" name="nomeProfessor" id="nomeProfessor" />
 
-<input type="submit" name="submit" id="submitt" value="Concluir">
+    <input type="submit" name="submit" id="sumbit" value="Salvar" />
+</form>
