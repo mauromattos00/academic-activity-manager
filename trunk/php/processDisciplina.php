a63c8a8ffaeb6e50664ea2b7f2b573161625e8fc
mauromattos00@gmail.com <mauromattos00@gmail.com@a50d1f8c-2995-2055-9274-b40652f596fe>
2014-06-28 21:00:05 +0000

    Aualização de gravação de dados Issue 7

--- trunk/php/processDisciplina.php
+++ trunk/php/processDisciplina.php
@@ -1,17 +1,26 @@
 <?php
+
 include'connection.php';
 
 $nomeDisciplina = isset($_POST['nomeDisciplina']) && $_POST['nomeDisciplina'] ? trim($_POST['nomeDisciplina']) : null;
 $nomeProfessor = isset($_POST['nomeProfessor']) && $_POST['nomeProfessor'] ? trim($_POST['nomeProfessor']) : null;
 
-if($nomeDisciplina){
-    $sql = (isset($_GET['id_usuario']) && ($id_usuario = $_GET['id_usuario']))
-    ? "UPDATE disciplina set nome='$nomeDisciplina' WHERE id_usuario = $id_usuario"
-    : "INSERT INTO disciplina (nome) VALUES ('$nomeDisciplina')";
+if($nomeProfessor){
+    $sql = (isset($_SESSION['id_usuario']) && ($id_usuario = $_SESSION['id_usuario']))
+            ? "UPDATE professor set nome='$nomeProfessor' WHERE id_usuario = $id_usuario"
+            : "INSERT INTO professor (nome, id_usuario) VALUES ('$nomeProfessor', '12')";
+}
+
+if ($nomeDisciplina) {
     
-    $result = mysql_query($sql, $database);    
+    $sql = (isset($_SESSION['id_usuario']) && ($id_usuario = $_SESSION['id_usuario']))
+            ? "UPDATE disciplina set nome='$nomeDisciplina' WHERE id_usuario = $id_usuario"
+            : "INSERT INTO disciplina (nome, id_usuario) VALUES ('$nomeDisciplina', '12')";
+
+    $result = mysql_query($sql, $database);
+
     if ($result) {
-        header('Location: ../index.php?pagina=formDisciplina&message=1');
+        header('Location: ../index.php?pagina=listadedisciplinas&message=1');
         exit;
     } else {
         header('Location: ../index.php?pagina=formDisciplina&message=2');
