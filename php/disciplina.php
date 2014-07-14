a68a3b609f1c7749e66f26045177a69971f7422b
mauromattos00 <mauromattos00@gmail.com>
2014-07-14 01:55:32 +0000

    Página para cada disciplina criada
    Issue 7

--- php/disciplina.php
+++ php/disciplina.php
@@ -1,3 +1,13 @@
-<?php
+<section>
+    <h1>Trabalhos</h1>
+    <?php while ($row = mysql_fetch_assoc($result)): ?>
+        <p><a class="linklista" href='index.php?pagina=trabalho&id_trabalho=<?php echo $row['id_trabalho'] ?>'><?php echo $row['nome'] ?></a></p>
+    <?php endwhile; ?>
+</section>
 
-?>
+<section>
+    <h1>Tarefas</h1>
+    <?php while ($row = mysql_fetch_assoc($result)): ?>
+        <p><a class="linklista" href='index.php?pagina=tarefa&id_tarefa=<?php echo $row['id_tarefa'] ?>'><?php echo $row['nome'] ?></a></p>
+    <?php endwhile; ?>
+</section>
